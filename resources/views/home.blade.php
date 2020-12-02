153ffe0223daf50094497e27c198638b75bdec99
Kevin <kevinmugiira@gmail.com>
2020-12-02 14:55:57 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,132 @@
+@extends('layout')
+
+
+
+@section('trans')
+
+
+
+    <section id="banner" data-video="images/banner">
+        <div class="inner">
+            <h1>Winner Restaurants</h1>
+            <p>A full responsive, business-oriented restaurants management system<br />
+                built by <a href="#">winning systems</a> and released under the <a href="#">Winning Systems Group</a>.</p>
+            <a href="#one" class="button special scrolly">Get Started</a>
+        </div>
+    </section>
+
+    <!-- One -->
+    <section id="one" class="wrapper style2">
+        <div class="inner">
+            <div>
+                <div class="box">
+                    <div class="image fit">
+                        <img src="assets/images/han(re).jpg" alt="" />
+                    </div>
+                    <div class="content">
+                        <header class="align-center">
+                            <h2>About Winning Systems</h2>
+                            <p>IT solutions & services for the Enterprise Business</p>
+                        </header>
+                        <hr />
+                        <p> Winning Systems is a group with focus on digital technologies. It uses the power of web, Analytics, Digital technologies and cloud to unlock the potential of business across verticals. We have a strong focus on positive user experience and employee happiness through unique, fully integrated, patented technology platforms.</p>
+                        <p>We serve Enterprises, ISV's and startup segments across the Retail, Financial service domanins & Healthcare. Development of digital power for business growth is our portion.Our business customers quickly transform to be long-term partners, as we delve deep to understand their business, and in the process, deliver long-lasting and effective solutions.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Two -->
+    <section id="two" class="wrapper style3">
+        <div class="inner">
+            <div id="flexgrid">
+                <div>
+                    <header>
+                        <h3>Delivery</h3>
+                    </header>
+                    <p>We make deliveries within every part of Nairobi and Kampala. Make any food orders of your choice from any of our restaurant affiliates and have it delivered any time of the day.</p>
+                    <ul class="actions">
+                        <li><a href="#" class="button alt">Learn More</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <header>
+                        <h3>Restaurants</h3>
+                    </header>
+                    <p>Our system is in use by restaurants both in Nairobi and Kampala. We have a range of amazing food places for you to explore. </p>
+                    <ul class="actions">
+                        <li><a href="#" class="button alt">Learn More</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <header>
+                        <h3>Winning Choices</h3>
+                    </header>
+                    <p>Winning systems provides a way to help your business grow away from the technicalities. We provide seminars for business staff to enable solidify your business' internal structure. </p>
+                    <ul class="actions">
+                        <li><a href="#" class="button alt">Learn More</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Three -->
+    <section id="three" class="wrapper style2">
+        <div class="inner">
+            <div class="grid-style">
+
+                <div>
+                    <div class="box">
+                        <div class="image fit">
+                            <img src="assets/images/nairobi2(re).jpg" alt="" />
+                        </div>
+                        <div class="content">
+                            <header class="align-center">
+                                <h2>Nairobi Winner Restaurants</h2>
+                                <p>The leading Restaurant system in Nairobi</p>
+                            </header>
+                            <hr />
+                            <p> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada. Praesent in sem ex. Morbi mattis sapien pretium tellus venenatis, at egestas urna ornare.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="box">
+                        <div class="image fit">
+                            <img src="assets/images/kampala1(re).jpg" alt="" />
+                        </div>
+                        <div class="content">
+                            <header class="align-center">
+                                <h2>Kampala Winner Restaurants</h2>
+                                <p>The leading Restaurant system in Kampala</p>
+                            </header>
+                            <hr />
+                            <p> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada. Praesent in sem ex. Morbi mattis sapien pretium tellus venenatis, at egestas urna ornare.</p>
+                            <p>Kampala Winner </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Four -->
+    <section id="four" class="wrapper style3">
+        <div class="inner">
+
+            <header class="align-center">
+                <h2>Morbi interdum mollis sapien</h2>
+                <p>Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada. Praesent in sem ex. Morbi mattis sapien pretium tellus venenatis, at egestas urna ornare.</p>
+            </header>
+
+        </div>
+    </section>
+
+
+
+
+@endsection
